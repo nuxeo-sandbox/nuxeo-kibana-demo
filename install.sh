b3302b51c55873f6cdbf4431b2a892d0229a0e61
Michaël Vachette <mvachette@nuxeo.com>
2015-02-26 16:39:17 +0100

    Minor fix

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 apt-get install apache2-utils supervisor -y
+a2enmod proxy_html
 cp apache/*.conf /etc/apache2/site-availables/
 a2ensite kibana
 service apache2 restart
-sudo cp supervisor/kibana4.conf /etc/supervisor/conf.d/
+sudo cp supervisor/kibana.conf /etc/supervisor/conf.d/
 mkdir /var/log/kibana
 chown ubuntu:ubuntu /var/log/kibana 
 service supervisor restart
