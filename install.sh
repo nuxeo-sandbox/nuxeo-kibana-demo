2813da891ac1e148ce03c2b7e42e9f32c99a320c
Thibaud Arguillere <ThibArg@users.noreply.github.com>
2015-07-20 13:26:45 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 apt-get install apache2-utils supervisor -y
 a2enmod proxy_html
-cp apache/*.conf /etc/apache2/site-availables/
+cp apache/*.conf /etc/apache2/sites-available/
 a2ensite kibana
 service apache2 restart
 sudo cp supervisor/kibana.conf /etc/supervisor/conf.d/
