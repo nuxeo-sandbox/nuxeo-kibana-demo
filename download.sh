bd611ac5f444cdb5000492b0bbbd3878ca8c6c55
Josh Fletcher <jfletcher@nuxeo.com>
2017-01-18 20:46:45 -0800

    Update for 8.10 (ES 2.3.5)

--- download.sh
+++ download.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-VERSION="4.1.1"
+VERSION="4.6.4"
+ARCH="x86_64"
 
 cd
-wget https://download.elasticsearch.org/kibana/kibana/kibana-$VERSION-linux-x64.tar.gz
-tar -xvf kibana-$VERSION-linux-x64.tar.gz
-mv  kibana-$VERSION-linux-x64 kibana
+wget https://download.elasticsearch.org/kibana/kibana/kibana-$VERSION-linux-$ARCH.tar.gz
+tar -xvf kibana-$VERSION-linux-$ARCH.tar.gz
+mv  kibana-$VERSION-linux-$ARCH kibana
