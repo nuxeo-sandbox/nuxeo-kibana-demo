d8eca48a02541e9d2728f25a07c1c20df7c0434b
Josh Fletcher <jfletcher@nuxeo.com>
2018-03-21 21:57:18 -0700

    Updated for 9.10 (ES 5.6.3)

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-VERSION="4.5.4"
-ARCH="x64"
+VERSION="5.6.3"
+ARCH="x86_64"
 
 cd
-wget https://download.elasticsearch.org/kibana/kibana/kibana-$VERSION-linux-$ARCH.tar.gz
+wget https://artifacts.elastic.co/downloads/kibana/kibana-$VERSION-linux-$ARCH.tar.gz
 tar -xvf kibana-$VERSION-linux-$ARCH.tar.gz
 mv  kibana-$VERSION-linux-$ARCH kibana
