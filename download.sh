dc8f4a7cee30f59b0c3b6b2fad9aaa809ae5bb48
Josh Fletcher <jfletcher@nuxeo.com>
2018-08-10 15:25:46 -0700

    Download OSS version instead

--- download.sh
+++ download.sh
@@ -3,7 +3,6 @@
 VERSION="6.3.0"
 ARCH="x86_64"
 
-cd
-wget https://artifacts.elastic.co/downloads/kibana/kibana-$VERSION-linux-$ARCH.tar.gz
+wget https://artifacts.elastic.co/downloads/kibana/kibana-oss-$VERSION-linux-$ARCH.tar.gz
 tar -xvf kibana-$VERSION-linux-$ARCH.tar.gz
 mv  kibana-$VERSION-linux-$ARCH kibana
