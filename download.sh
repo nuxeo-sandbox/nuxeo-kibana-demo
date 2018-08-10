196161f9af622985f6e7368f6b00ba0b596aefe7
Josh Fletcher <jfletcher@nuxeo.com>
2018-08-10 15:31:38 -0700

    Fix bad paths

--- download.sh
+++ download.sh
@@ -4,5 +4,5 @@ VERSION="6.3.0"
 ARCH="x86_64"
 
 wget https://artifacts.elastic.co/downloads/kibana/kibana-oss-$VERSION-linux-$ARCH.tar.gz
-tar -xvf kibana-$VERSION-linux-$ARCH.tar.gz
-mv  kibana-$VERSION-linux-$ARCH kibana
+tar -xvf kibana-oss-$VERSION-linux-$ARCH.tar.gz
+mv  kibana-oss-$VERSION-linux-$ARCH kibana
