87cdd7354afd7c7310d2bd58a6222ea2c4b4fc04
Josh Fletcher <jfletcher@nuxeo.com>
2018-08-10 15:55:44 -0700

    Accidentally deleted this.

--- download.sh
+++ download.sh
@@ -3,6 +3,7 @@
 VERSION="6.3.0"
 ARCH="x86_64"
 
+cd
 wget https://artifacts.elastic.co/downloads/kibana/kibana-oss-$VERSION-linux-$ARCH.tar.gz
 tar -xvf kibana-oss-$VERSION-linux-$ARCH.tar.gz
 mv  kibana-$VERSION-linux-$ARCH kibana
